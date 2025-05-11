06aec45857f619c6245ef5f0459a477dcc866ca8
Heithor <contato.hecthorur@gmail.com>
2025-05-11 20:18:43 -0300

    fix: corrigindo identação

--- educational-institution-project/microservice-architecture/institution-core-service/src/domain/teacher/repositories/TeacherRepository.js
+++ educational-institution-project/microservice-architecture/institution-core-service/src/domain/teacher/repositories/TeacherRepository.js
@@ -1,10 +1,9 @@
 export class TeacherRepository {
-    async save(student) {
-      throw new Error('Method not implemented');
-    }
-  
-    async findByEmail(email) {
-      throw new Error('Method not implemented');
-    }
+  async save(student) {
+    throw new Error('Method not implemented');
   }
-  
+
+  async findByEmail(email) {
+    throw new Error('Method not implemented');
+  }
+}
